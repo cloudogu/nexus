64177423bd12c2bef345cfcd87d0a5370d07eca1
Robert Auer <robert.auer@cloudogu.com>
2018-04-13 13:39:53 +0200

    Configure Nexus according to nexusConfiguration.json at startup

--- resources/startup.sh
+++ resources/startup.sh
@@ -4,77 +4,126 @@ set -o nounset
 set -o pipefail
 
 # variables
-ADMUSR="admin"
-ADMDEFAULTPW="admin123"
-ADMINGROUP=$(doguctl config --global admin_group)
-DOMAIN=$(doguctl config --global domain)
-MAIL_ADDRESS=$(doguctl config -d "nexus@${DOMAIN}" --global mail_address)
-FQDN=$(doguctl config --global fqdn)
+# ADMUSR="admin"
+# ADMDEFAULTPW="admin123"
+# ADMINGROUP=$(doguctl config --global admin_group)
+# DOMAIN=$(doguctl config --global domain)
+# MAIL_ADDRESS=$(doguctl config -d "nexus@${DOMAIN}" --global mail_address)
+# FQDN=$(doguctl config --global fqdn)
 NEXUS_DATA_DIR=/var/lib/nexus
+NEXUS_PID=0
 
-function set_random_admin_password {
-  ADMPW=$(doguctl random)
-  curl -s --retry 3 --retry-delay 10 -X POST -H "Content-Type: application/json" -d "{data:{userId:\"$ADMUSR\",newPassword:\"$ADMPW\"}}" --insecure 'http://127.0.0.1:8081/nexus/service/local/users_setpw' -u "$ADMUSR":"$ADMDEFAULTPW"
-  doguctl config -e "admin_password" "${ADMPW}"
-  echo "${ADMPW}"
-}
+# function set_random_admin_password {
+#   ADMPW=$(doguctl random)
+#   curl -s --retry 3 --retry-delay 10 -X POST -H "Content-Type: application/json" -d "{data:{userId:\"$ADMUSR\",newPassword:\"$ADMPW\"}}" --insecure 'http://127.0.0.1:8081/nexus/service/local/users_setpw' -u "$ADMUSR":"$ADMDEFAULTPW"
+#   doguctl config -e "admin_password" "${ADMPW}"
+#   echo "${ADMPW}"
+# }
+#
+# function render_template(){
+#   FILE="$1"
+#   if [ ! -f "$FILE" ]; then
+#     echo >&2 "could not find template $FILE"
+#     exit 1
+#   fi
+#
+#   # render template
+#   eval "echo \"$(cat $FILE)\""
+# }
+#
+# function setProxyConfiguration(){
+#   NEXUS_CONFIGURATION=$(curl -s -H 'content-type:application/json' -H 'accept:application/json' 'http://127.0.0.1:8081/nexus/service/local/global_settings/current' -u "$ADMUSR":"$ADMPW")
+#   # Write proxy settings if enabled in etcd
+#   if [ "true" == "$(doguctl config --global proxy/enabled)" ]; then
+#     if PROXYSERVER=$(doguctl config --global proxy/server) && PROXYPORT=$(doguctl config --global proxy/port); then
+#       writeProxyCredentialsTo "${NEXUS_CONFIGURATION}"
+#       if PROXYUSER=$(doguctl config --global proxy/username) && PROXYPASSWORD=$(doguctl config --global proxy/password); then
+#         writeProxyAuthenticationCredentialsTo "${NEXUS_CONFIGURATION}"
+#       else
+#         echo "Proxy authentication credentials are incomplete or not existent."
+#       fi
+#       putNexusConfiguration
+#     else
+#       echo "Proxy server or port configuration missing in etcd."
+#     fi
+#   else
+#     PROXYSERVER=""
+#     PROXYPORT=0
+#     PROXYUSER=""
+#     PROXYPASSWORD=""
+#     writeProxyCredentialsTo "${NEXUS_CONFIGURATION}"
+#     writeProxyAuthenticationCredentialsTo "${NEXUS_CONFIGURATION}"
+#     putNexusConfiguration
+#   fi
+# }
+#
+# function writeProxyCredentialsTo(){
+#   NEXUS_CONFIGURATION=$(echo "$1" | jq ".data.remoteProxySettings.httpProxySettings+={\"proxyHostname\": \"${PROXYSERVER}\"}")
+#   NEXUS_CONFIGURATION=$(echo "${NEXUS_CONFIGURATION}" | jq ".data.remoteProxySettings.httpProxySettings+={\"proxyPort\": ${PROXYPORT}}")
+# }
+#
+# function writeProxyAuthenticationCredentialsTo(){
+#   # Add proxy authentication credentials
+#   NEXUS_CONFIGURATION=$(echo "$1" | jq ".data.remoteProxySettings.httpProxySettings.authentication+={\"username\": \"${PROXYUSER}\"}")
+#   NEXUS_CONFIGURATION=$(echo "${NEXUS_CONFIGURATION}" | jq ".data.remoteProxySettings.httpProxySettings.authentication+={\"password\": \"${PROXYPASSWORD}\"}")
+# }
+#
+# function putNexusConfiguration(){
+#   curl -s --retry 3 --retry-delay 10 -H "Content-Type: application/json" -X PUT -d "${NEXUS_CONFIGURATION}" "http://127.0.0.1:8081/nexus/service/local/global_settings/current" -u "$ADMUSR":"$ADMPW"
+# }
 
-function render_template(){
-  FILE="$1"
-  if [ ! -f "$FILE" ]; then
-    echo >&2 "could not find template $FILE"
-    exit 1
-  fi
-
-  # render template
-  eval "echo \"$(cat $FILE)\""
-}
-
-function setProxyConfiguration(){
-  NEXUS_CONFIGURATION=$(curl -s -H 'content-type:application/json' -H 'accept:application/json' 'http://127.0.0.1:8081/nexus/service/local/global_settings/current' -u "$ADMUSR":"$ADMPW")
-  # Write proxy settings if enabled in etcd
-  if [ "true" == "$(doguctl config --global proxy/enabled)" ]; then
-    if PROXYSERVER=$(doguctl config --global proxy/server) && PROXYPORT=$(doguctl config --global proxy/port); then
-      writeProxyCredentialsTo "${NEXUS_CONFIGURATION}"
-      if PROXYUSER=$(doguctl config --global proxy/username) && PROXYPASSWORD=$(doguctl config --global proxy/password); then
-        writeProxyAuthenticationCredentialsTo "${NEXUS_CONFIGURATION}"
-      else
-        echo "Proxy authentication credentials are incomplete or not existent."
-      fi
-      putNexusConfiguration
+function configureNexus() {
+  echo "Configuring Nexus..."
+  if [[ -e /opt/sonatype/nexus/resources/nexusConfiguration.json ]]; then
+    echo "Posting configuration script to Nexus..."
+    curl --silent --insecure -X POST --no-buffer --user admin:admin123 --header 'Content-Type: application/json' --header 'Accept: application/json' -d @/opt/sonatype/nexus/resources/nexusConfiguration.json 'https://192.168.56.2/nexus/service/rest/v1/script'
+    CONFIG_STATUS=$(curl --silent --head --no-buffer --insecure --user admin:admin123 https://192.168.56.2/nexus/service/rest/v1/script/nexusConfiguration)
+    CONFIG_STATUS_CODE=$(echo "$CONFIG_STATUS"|head -n 1|cut -d$' ' -f2)
+    if [[ ${CONFIG_STATUS_CODE} != 200 ]]; then
+      echo "Nexus configuration script has not been posted successfully"
+      exit 1
     else
-      echo "Proxy server or port configuration missing in etcd."
+       echo "Executing configuration script..."
+       SCRIPT_EXECUTION=$(curl --silent --head --insecure --no-buffer -X POST --user admin:admin123 --header 'Content-Type: text/plain' --header 'Accept: application/json' 'https://192.168.56.2/nexus/service/rest/v1/script/nexusConfiguration/run')
+       SCRIPT_STATUS_CODE=$(echo "${SCRIPT_EXECUTION}"|head -n 1|cut -d$' ' -f2)
+       if [[ ${SCRIPT_STATUS_CODE} != 200 ]]; then
+         echo "Configuration script has not been executed successfully"
+         exit 1
+       else
+         echo "Removing /opt/sonatype/nexus/resources/nexusConfiguration.json"
+         rm /opt/sonatype/nexus/resources/nexusConfiguration.json
+       fi
     fi
   else
-    PROXYSERVER=""
-    PROXYPORT=0
-    PROXYUSER=""
-    PROXYPASSWORD=""
-    writeProxyCredentialsTo "${NEXUS_CONFIGURATION}"
-    writeProxyAuthenticationCredentialsTo "${NEXUS_CONFIGURATION}"
-    putNexusConfiguration
+    echo "Configuration file doesn't exist"
   fi
 }
 
-function writeProxyCredentialsTo(){
-  NEXUS_CONFIGURATION=$(echo "$1" | jq ".data.remoteProxySettings.httpProxySettings+={\"proxyHostname\": \"${PROXYSERVER}\"}")
-  NEXUS_CONFIGURATION=$(echo "${NEXUS_CONFIGURATION}" | jq ".data.remoteProxySettings.httpProxySettings+={\"proxyPort\": ${PROXYPORT}}")
-}
-
-function writeProxyAuthenticationCredentialsTo(){
-  # Add proxy authentication credentials
-  NEXUS_CONFIGURATION=$(echo "$1" | jq ".data.remoteProxySettings.httpProxySettings.authentication+={\"username\": \"${PROXYUSER}\"}")
-  NEXUS_CONFIGURATION=$(echo "${NEXUS_CONFIGURATION}" | jq ".data.remoteProxySettings.httpProxySettings.authentication+={\"password\": \"${PROXYPASSWORD}\"}")
-}
-
-function putNexusConfiguration(){
-  curl -s --retry 3 --retry-delay 10 -H "Content-Type: application/json" -X PUT -d "${NEXUS_CONFIGURATION}" "http://127.0.0.1:8081/nexus/service/local/global_settings/current" -u "$ADMUSR":"$ADMPW"
+function stopNexus() {
+  echo "Stopping Nexus..."
+  kill ${NEXUS_PID}
+  wait ${NEXUS_PID}
 }
 
 function startNexusAndWaitForHealth(){
-  $START_NEXUS &
-  if ! doguctl wait --port 8081 --timeout 120; then
-    echo "Nexus seems not to be started. Exiting."
+  echo "Starting Nexus and waiting for healthy state..."
+  ${NEXUS_WORKDIR}/bin/nexus run &
+  NEXUS_PID=$!
+  END=$((SECONDS+120))
+  NEXUS_IS_HEALTHY=false
+  while [ $SECONDS -lt $END ]; do
+    CURL_HEALTH_STATUS=$(curl --silent --head --no-buffer --insecure --user admin:admin123 https://192.168.56.2/nexus/service/metrics/healthcheck)
+    HEALTH_STATUS_CODE=$(echo "$CURL_HEALTH_STATUS"|head -n 1|cut -d$' ' -f2)
+    if [[ ${HEALTH_STATUS_CODE} != 200 ]]; then
+      sleep 5
+    else
+      echo "Nexus is healthy now"
+      NEXUS_IS_HEALTHY=true
+      break
+    fi
+  done
+  if [[ ! $NEXUS_IS_HEALTHY ]]; then
+    echo "Nexus did not reach healthy state in 120 seconds"
     exit 1
   fi
 }
@@ -113,9 +162,15 @@ cat <<EOF > ${NEXUS_DATA_DIR}/etc/nexus.properties
 nexus-context-path=/nexus
 EOF
 
+startNexusAndWaitForHealth
+
+configureNexus
+
+stopNexus
+
 doguctl state ready
 
-echo "Running nexus..."
+echo "Running Nexus..."
 ${NEXUS_WORKDIR}/bin/nexus run
 
 #
