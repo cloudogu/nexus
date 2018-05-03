80de241200e95c61dafb2ed145272ffce2fdcbb8
Robert Auer <robert.auer@cloudogu.com>
2018-05-03 14:29:45 +0200

    Remove browser interaction from REST permissions test

--- integrationTests/permissions-rest.spec.js
+++ integrationTests/permissions-rest.spec.js
@@ -7,12 +7,16 @@ jest.setTimeout(30000);
 let driver;
 let adminFunctions;
 
+const testUserName = 'testUser';
+const testUserEmail = "testUser@test.de"
+const testUserPassword = "testuserpassword"
+
 // disable certificate validation
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 beforeEach(async() => {
     driver = utils.createDriver(webdriver);
-    adminFunctions = new AdminFunctions('testUserR', 'testUserR', 'testUserR', 'testUserR@test.de', 'testuserrpasswort');
+    adminFunctions = new AdminFunctions(testUserName, testUserName, testUserName, testUserEmail, testUserPassword);
     await adminFunctions.createUser();
 });
 
@@ -24,32 +28,20 @@ afterEach(async() => {
 
 describe('administration rest tests', () => {
 
-    test('rest - user (testUser) has admin privileges', async() => {
-        await driver.get(utils.getCasUrl(driver));
+    test('user (' + testUserName + ') has admin privileges', async() => {
         await adminFunctions.giveAdminRights();
-        await adminFunctions.testUserLogin(driver);
-        await adminFunctions.logoutViaCasEndpoint(driver);
         await adminFunctions.accessScriptingAPI(200);
     });
 
-    test('rest - user (testUser) has no admin privileges', async() => {
-        await driver.get(utils.getCasUrl(driver));
-        await adminFunctions.testUserLogin(driver);
-        await adminFunctions.logoutViaCasEndpoint(driver);
+    test('user (' + testUserName + ') has no admin privileges', async() => {
         await adminFunctions.accessScriptingAPI(403);
     });
 
 
-    test('rest - user (testUser) remove admin privileges', async() => {
-        await driver.get(utils.getCasUrl(driver));
+    test('user (' + testUserName + ') remove admin privileges', async() => {
         await adminFunctions.giveAdminRights();
-        await adminFunctions.testUserLogin(driver);
-        await adminFunctions.logoutViaCasEndpoint(driver);
         await adminFunctions.accessScriptingAPI(200);
         await adminFunctions.takeAdminRights();
-        await driver.get(utils.getCasUrl(driver));
-        await adminFunctions.testUserLogin(driver);
-        await adminFunctions.logoutViaCasEndpoint(driver);
         await adminFunctions.accessScriptingAPI(403);
     });
 
