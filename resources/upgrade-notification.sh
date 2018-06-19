28a618db18ddf3e66d15eaf2405daff7cb15495c
CloudoguNguyen <thanh.long.nguyen@cloudogu.com>
2018-06-19 11:34:42 +0200

    Updated upgrade-notification.sh

--- resources/upgrade-notification.sh
+++ resources/upgrade-notification.sh
@@ -7,6 +7,9 @@ set -o pipefail
 FROM_VERSION="${1}"
 TO_VERSION="${2}"
 
-if [ "${FROM_VERSION}" == "2*" ] && "${TO_VERSION}" == "3*"; then
-  echo "CAUTION! Upgrading to nexus 3 now will delete every datas from nexus 2. You won't be able to recreate them. Please ensure you have backed up your data."
+if [[ $FROM_VERSION == 2* ]] && [[ $TO_VERSION == 3* ]]; then
+    YELLOW='\033[1;33m'
+    NC='\033[0m'
+    printf "${YELLOW}~~~~CAUTION~~~~\n"
+    printf "${NC}Upgrading to nexus 3 now will delete every data from nexus 2. You won't be able to recreate them. Please ensure you have backed up your data.\n"
 fi
