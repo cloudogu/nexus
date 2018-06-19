e72192c9b20c1ea6485674547f395e2fbc4072a2
CloudoguNguyen <thanh.long.nguyen@cloudogu.com>
2018-06-19 08:48:34 +0200

    Updated dogu.json and upgrade-notification.sh

--- resources/upgrade-notification.sh
+++ resources/upgrade-notification.sh
@@ -4,6 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-version = "$(docker images registry.cloudogu.com/official/nexus --format \"{{.Tag}}\")"
+FROM_VERSION="${1}"
+TO_VERSION="${2}"
 
-echo "${version}"
+if [ "${FROM_VERSION}" == "2*" ] && "${TO_VERSION}" == "3*"; then
+  echo "CAUTION! Upgrading to nexus 3 now will delete every datas from nexus 2. You won't be able to recreate them. Please ensure you have backed up your data."
+fi
