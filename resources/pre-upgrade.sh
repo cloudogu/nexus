650e10dd49ac9aef9aeb101e9df1bd94a50219cb
Maren Süwer <maren.suewer@cloudogu.com>
2018-06-19 15:09:29 +0200

    change usage of variable to file since variable is not global

--- resources/pre-upgrade.sh
+++ resources/pre-upgrade.sh
@@ -5,14 +5,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+export MIGRATION_FROM_2_TO_3=false;
+NEXUS_DATA_DIR=/var/lib/nexus
 
 echo "pre-upgrade script is running"
-#cp -r ${NEXUS_DATA_DIR} /var/lib/backupNexus
 
 FROM_VERSION="${1}"
 TO_VERSION="${2}"
 
 if [[ $FROM_VERSION == 2* ]] && [[ $TO_VERSION == 3* ]]; then
     echo "setting flat"
-    MIGRATION_FROM_2_TO_3=true
+    touch "${NEXUS_DATA_DIR}"/nexusFullBackup.dump
 fi
