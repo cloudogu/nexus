525c55b5479a254aae3d2a6e8bc3bda70d5918b0
Jasper <jasper.bartscher@cloudogu.com>
2022-07-27 14:24:28 +0200

    Fix update of cleanup policy.
    Couldn't make the update process work so the cleanup policy is now
    deleted and newly created each time.

--- resources/opt/sonatype/nexus/resources/nexusSetupCleanupPolicies.groovy
+++ resources/opt/sonatype/nexus/resources/nexusSetupCleanupPolicies.groovy
@@ -21,36 +21,34 @@ def String asStringSeconds(String daysString) {
  * @return
  */
 def createMavenSnapshotCleanupPolicy(configurationParameters) {
-    try {
-        def policyStorage = container.lookup(CleanupPolicyStorage.class.getName())
+    def policyStorage = container.lookup(CleanupPolicyStorage.class.getName())
 
-        def cleanupPolicy = policyStorage.newCleanupPolicy()
-        cleanupPolicy.setName(configurationParameters.name)
-        cleanupPolicy.setNotes(configurationParameters.notes)
-        cleanupPolicy.setMode(configurationParameters.mode)
-        cleanupPolicy.setFormat(configurationParameters.format)
+    def cleanupPolicy = policyStorage.newCleanupPolicy()
+    cleanupPolicy.setName(configurationParameters.name)
+    cleanupPolicy.setNotes(configurationParameters.notes)
+    cleanupPolicy.setMode(configurationParameters.mode)
+    cleanupPolicy.setFormat(configurationParameters.format)
 
-        def criteriaMap = [:]
-        criteriaMap.put(REGEX_KEY, configurationParameters.criteria.regex) // criteriaAssetRegex
-        if (configurationParameters.criteria.criteriaReleaseType != "") {
-            // We do a additional check here as the criteriaReleaseType is not supported for every kind of repository
-            // and should therefore be only set if a value is present.
-            // see: https://help.sonatype.com/repomanager3/nexus-repository-administration/repository-management/cleanup-policies
-            criteriaMap.put(IS_PRERELEASE_KEY, "PRERELEASES".equals(configurationParameters.criteria.criteriaReleaseType).toString())
-        }
+    def criteriaMap = [:]
+    criteriaMap.put(REGEX_KEY, configurationParameters.criteria.regex) // criteriaAssetRegex
+    if (configurationParameters.criteria.criteriaReleaseType != "") {
+        // We do a additional check here as the criteriaReleaseType is not supported for every kind of repository
+        // and should therefore be only set if a value is present.
+        // see: https://help.sonatype.com/repomanager3/nexus-repository-administration/repository-management/cleanup-policies
+        criteriaMap.put(IS_PRERELEASE_KEY, "PRERELEASES".equals(configurationParameters.criteria.criteriaReleaseType).toString())
+    }
+    criteriaMap.put(LAST_BLOB_UPDATED_KEY, asStringSeconds(configurationParameters.criteria.criteriaLastBlobUpdated))
 
-        criteriaMap.put(LAST_BLOB_UPDATED_KEY, asStringSeconds(configurationParameters.criteria.criteriaLastBlobUpdated))
-        // criteriaLastBlobUpdated
-        cleanupPolicy.setCriteria(criteriaMap)
+    cleanupPolicy.setCriteria(criteriaMap)
 
-        if (policyStorage.exists(configurationParameters.name)) {
-            log.info("update cleanup policy")
-            policyStorage.update(cleanupPolicy)
-        } else {
-            policyStorage.add(cleanupPolicy)
-        }
-    } catch (e) {
-        log.info("An error occurred while creating the cleanup policy " + configurationParameters.name + ". Skipping...")
+    deleteCleanupPolicyIfExists(configurationParameters.name)
+    policyStorage.add(cleanupPolicy)
+}
+
+def deleteCleanupPolicyIfExists(String name) {
+    def cleanupPolicyStorage = container.lookup(CleanupPolicyStorage.class.getName())
+    if (cleanupPolicyStorage.exists(name)) {
+        cleanupPolicyStorage.remove(cleanupPolicyStorage.get(name))
     }
 }
 
