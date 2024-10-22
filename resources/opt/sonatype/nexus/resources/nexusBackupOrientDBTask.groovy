f6bd3087c035001ee2255c35a0fa81275a7dd06d
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2024-10-22 08:17:26 +0200

    # 146 insert Backup skript

--- resources/opt/sonatype/nexus/resources/nexusBackupOrientDBTask.groovy
+++ resources/opt/sonatype/nexus/resources/nexusBackupOrientDBTask.groovy
@@ -1,12 +1,26 @@
 import org.sonatype.nexus.scheduling.TaskConfiguration
 import org.sonatype.nexus.scheduling.TaskSupport
-// def taskScheduler = container.lookup(TaskScheduler.class.getName())
-// def existingTasks = taskScheduler.listsTasks().findAll { it.getTypeId() == configurationParameters.type && it.getName() == configurationParameters.name }
-// existingTasks.collect { it.remove() }
-def config = taskScheduler.createTaskConfigurationInstance("db.backup")
-config.setEnabled("enabled")
-config.setName("testname1")
-config.setString("location", "/var/lib/nexus")
-def task = new TaskSupport(true)
-task.configure(config)
-task.call()
+import groovy.json.JsonSlurper
+
+
+// get parameters from payload JSON file
+def configurationParameters = new JsonSlurper().parseText(args)
+
+def createBackupOrientDBTask(configurationParameters) {
+    def taskScheduler = container.lookup(TaskScheduler.class.getName())
+    def existingTasks = taskScheduler.listsTasks().findAll { it.getTypeId() == configurationParameters.type && it.getName() == configurationParameters.name }
+    existingTasks.collect { it.remove() }
+
+    TaskConfiguration config = taskScheduler.createTaskConfigurationInstance("db.backup")
+    config.setEnabled("enabled")
+    config.setName("testname1")
+    config.setString("location", "/var/lib/nexus")
+
+    Calendar today = Calendar.getInstance();
+    today.clear(Calendar.HOUR); today.clear(Calendar.MINUTE); today.clear(Calendar.SECOND);
+    Date todayDate = today.getTime();
+
+    taskScheduler.scheduleTask(config, taskScheduler.scheduleFactory.cron(todayDate, configurationParameters.cron))
+}
+
+createBackupOrientDBTask(configurationParameters)
