3ea9ab88146c0d70bc5df3dce839ba21cc8e5332
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2024-10-22 09:32:45 +0200

    # 146 wait for backup files

--- resources/pre-upgrade.sh
+++ resources/pre-upgrade.sh
@@ -7,7 +7,6 @@ set -o pipefail
 
 # shellcheck disable=SC1091
 source /util.sh
-source /versions.sh
 
 NEXUS_DATA_DIR=/var/lib/nexus
 MIGRATION_FILE_NAME="opt/sonatype/nexus/orient_backup.bak"
@@ -20,6 +19,92 @@ echo "Executing pre upgrade"
 if [[ $FROM_VERSION == 2* ]] && [[ $TO_VERSION == 3* ]]; then
     touch "${NEXUS_DATA_DIR}"/migration
 fi
+
+MAJOR_VERSION_INDEX=1
+MINOR_VERSION_INDEX=2
+BUGFIX_VERSION_INDEX=3
+DOGU_VERSION_INDEX=4
+
+# Returns true if X is less than or equal to Y; otherwise false
+function versionXLessOrEqualThanY() {
+  local sourceVersion="${1}"
+  local targetVersion="${2}"
+
+  if [[ "${sourceVersion}" == "${targetVersion}" ]]; then
+    return 0
+  fi
+
+  local sourceMajor=0 sourceMinor=0 sourceBugfix=0 sourceDogu=0 \
+        targetMajor=0 targetMinor=0 targetBugfix=0 targetDogu=0 \
+        majorLessOrEqual=0 minorLessOrEqual=0 bugfixLessOrEqual=0
+
+  if ! is_valid_version "${sourceVersion}"; then
+    echo "ERROR: source dogu version ${sourceVersion} does not seem to be a semantic version"
+    exit 1
+  fi
+
+  if ! is_valid_version "${targetVersion}"; then
+    echo "ERROR: target dogu version ${targetVersion} does not seem to be a semantic version"
+    exit 1
+  fi
+
+  sourceMajor="$(get_version "${sourceVersion}" "${MAJOR_VERSION_INDEX}")"
+  targetMajor="$(get_version "${targetVersion}" "${MAJOR_VERSION_INDEX}")"
+  if [[ $((sourceMajor)) -lt $((targetMajor)) ]]; then
+    return 0
+  fi
+
+  majorLessOrEqual="$(versionXLessOrEqualThanY "${sourceMajor}" "${targetMajor}")"
+  sourceMinor="$(get_version "${sourceVersion}" "${MINOR_VERSION_INDEX}")"
+  targetMinor="$(get_version "${targetVersion}" "${MINOR_VERSION_INDEX}")"
+  if [[ $((sourceMinor)) -lt $((targetMinor)) ]] && [[ ! $majorLessOrEqual ]]; then
+    return 0
+  fi
+
+  minorLessOrEqual="$(versionXLessOrEqualThanY "${sourceMinor}" "${targetMinor}")"
+  sourceBugfix="$(get_version "${sourceVersion}" "${BUGFIX_VERSION_INDEX}")"
+  targetBugfix="$(get_version "${targetVersion}" "${BUGFIX_VERSION_INDEX}")"
+  if [[ $((sourceBugfix)) -lt $((targetBugfix)) ]] && [[ ! $majorLessOrEqual ]] && [[ ! $minorLessOrEqual ]]; then
+    return 0
+  fi
+
+  bugfixLessOrEqual="$(versionXLessOrEqualThanY "${sourceBugfix}" "${targetBugfix}")"
+  sourceDogu="$(get_version "${sourceVersion}" "${DOGU_VERSION_INDEX}")"
+  targetDogu="$(get_version "${targetVersion}" "${DOGU_VERSION_INDEX}")"
+  if [[ $((sourceDogu)) -lt $((targetDogu)) ]] && [[ ! $majorLessOrEqual ]] && [[ ! $minorLessOrEqual ]] && [[ ! $bugfixLessOrEqual ]]; then
+    return 0
+  fi
+
+  return 1
+}
+
+# get_version returns the value of the given semver index. If the input is not a valid semantic version
+# the function exits with exit code 1
+# MAJOR_VERSION  -> 1
+# MINOR_VERSION  -> 2
+# BUGFIX_VERSION -> 3
+# DOGU_VERSION   -> 4
+get_version() {
+  local version index value
+  version="${1}"
+  index="${2}"
+  value="0"
+  if ! is_valid_version "${version}"; then
+    echo "ERROR: dogu version ${version} does not seem to be a semantic version"
+    exit 1
+  fi
+  value=${BASH_REMATCH[index]}
+  echo "${value}"
+}
+
+is_valid_version() {
+  local version
+  version="${1}"
+  declare -r semVerRegex='([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)'
+  if [[ ! ${version} =~ ${semVerRegex} ]]; then
+    return 1
+  fi
+}
 
 writeDatabaseBackupScriptToFile() {
   echo 'import org.sonatype.nexus.scheduling.TaskConfiguration; import org.sonatype.nexus.scheduling.TaskScheduler; def taskScheduler = container.lookup(TaskScheduler.class.getName()); TaskConfiguration config = taskScheduler.createTaskConfigurationInstance("db.backup"); config.setEnabled(true); config.setName("orientDatabaseBackup"); config.setString("location", "/var/lib/nexus"); taskScheduler.submit(config); def youSaid = args; return "Hello. You said: " + youSaid;' > "${NEXUS_WORKDIR}/resources/nexusBackupOrientDBTask.groovy"
@@ -37,7 +122,8 @@ if versionXLessOrEqualThanY "${FROM_VERSION}" "3.70.2-3" && ! versionXLessOrEqua
   writeDatabaseBackupScriptToFile
 
   NEXUS_URL="http://localhost:8081/nexus" NEXUS_USER="$(doguctl config -e admin_user)" NEXUS_PASSWORD="$(doguctl config -e admin_pw)" nexus-scripting execute "${NEXUS_WORKDIR}/resources/nexusBackupOrientDBTask.groovy"
-  while [ ! -e "${NEXUS_DATA_DIR}/*.bak" ]
+  # wait for backup files to appear
+  while [ ! -f "${NEXUS_DATA_DIR}/analytics-*.bak" && ! -f "${NEXUS_DATA_DIR}/component-*.bak" && ! -f "${NEXUS_DATA_DIR}/config-*.bak" && ! -f "${NEXUS_DATA_DIR}/security-*.bak"]
   do
       sleep .6
   done
