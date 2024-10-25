5f4ea0675ebc23d88b9f98166cc72e1d3a2b3b30
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2024-10-25 07:52:04 +0200

    # 146 change version comparison

--- resources/upgrade-notification.sh
+++ resources/upgrade-notification.sh
@@ -35,91 +35,20 @@ Most notably, if you are using the CES user management, make sure that the admin
     printf "\nFor additional support or questions, feel free to contact hello@cloudogu.com.\n"
 fi
 
-MAJOR_VERSION_INDEX=1
-MINOR_VERSION_INDEX=2
-BUGFIX_VERSION_INDEX=3
-DOGU_VERSION_INDEX=4
-
-# Returns true if X is less than or equal to Y; otherwise false
-function versionXLessOrEqualThanY() {
-  local sourceVersion="${1}"
-  local targetVersion="${2}"
-
-  if [[ "${sourceVersion}" == "${targetVersion}" ]]; then
-    return 0
-  fi
-
-  local sourceMajor=0 sourceMinor=0 sourceBugfix=0 sourceDogu=0 \
-        targetMajor=0 targetMinor=0 targetBugfix=0 targetDogu=0 \
-        majorLessOrEqual=0 minorLessOrEqual=0 bugfixLessOrEqual=0
-
-  if ! is_valid_version "${sourceVersion}"; then
-    echo "ERROR: source dogu version ${sourceVersion} does not seem to be a semantic version"
-    exit 1
-  fi
-
-  if ! is_valid_version "${targetVersion}"; then
-    echo "ERROR: target dogu version ${targetVersion} does not seem to be a semantic version"
-    exit 1
-  fi
-
-  sourceMajor="$(get_version "${sourceVersion}" "${MAJOR_VERSION_INDEX}")"
-  targetMajor="$(get_version "${targetVersion}" "${MAJOR_VERSION_INDEX}")"
-  if [[ $((sourceMajor)) -lt $((targetMajor)) ]]; then
-    return 0
-  fi
-
-  majorLessOrEqual="$(versionXLessOrEqualThanY "${sourceMajor}" "${targetMajor}")"
-  sourceMinor="$(get_version "${sourceVersion}" "${MINOR_VERSION_INDEX}")"
-  targetMinor="$(get_version "${targetVersion}" "${MINOR_VERSION_INDEX}")"
-  if [[ $((sourceMinor)) -lt $((targetMinor)) ]] && [[ ! $majorLessOrEqual ]]; then
-    return 0
-  fi
-
-  minorLessOrEqual="$(versionXLessOrEqualThanY "${sourceMinor}" "${targetMinor}")"
-  sourceBugfix="$(get_version "${sourceVersion}" "${BUGFIX_VERSION_INDEX}")"
-  targetBugfix="$(get_version "${targetVersion}" "${BUGFIX_VERSION_INDEX}")"
-  if [[ $((sourceBugfix)) -lt $((targetBugfix)) ]] && [[ ! $majorLessOrEqual ]] && [[ ! $minorLessOrEqual ]]; then
-    return 0
-  fi
-
-  bugfixLessOrEqual="$(versionXLessOrEqualThanY "${sourceBugfix}" "${targetBugfix}")"
-  sourceDogu="$(get_version "${sourceVersion}" "${DOGU_VERSION_INDEX}")"
-  targetDogu="$(get_version "${targetVersion}" "${DOGU_VERSION_INDEX}")"
-  if [[ $((sourceDogu)) -lt $((targetDogu)) ]] && [[ ! $majorLessOrEqual ]] && [[ ! $minorLessOrEqual ]] && [[ ! $bugfixLessOrEqual ]]; then
-    return 0
-  fi
-
-  return 1
+versionXLaterOrEqualThanY() {
+    printf '%s\n' "$1" "$2" | sort -C -V
 }
 
-# get_version returns the value of the given semver index. If the input is not a valid semantic version
-# the function exits with exit code 1
-# MAJOR_VERSION  -> 1
-# MINOR_VERSION  -> 2
-# BUGFIX_VERSION -> 3
-# DOGU_VERSION   -> 4
-get_version() {
-  local version index value
-  version="${1}"
-  index="${2}"
-  value="0"
-  if ! is_valid_version "${version}"; then
-    echo "ERROR: dogu version ${version} does not seem to be a semantic version"
-    exit 1
-  fi
-  value=${BASH_REMATCH[index]}
-  echo "${value}"
+versionXLaterThanY() {
+    ! verlte "$2" "$1"
 }
 
-is_valid_version() {
-  local version
-  version="${1}"
-  declare -r semVerRegex='([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)'
-  if [[ ! ${version} =~ ${semVerRegex} ]]; then
-    return 1
-  fi
+versionXBetweenYZ() {
+    # args: min, actual, max
+    printf '%s\n' "$@" | sort -C -V
 }
+
+echo "test upgrade notification"
 
 if [[ "${FROM_VERSION}" == 3.70.2* ]] && [[ "${TO_VERSION}" == 3.73.* ]]; then
     RED='\033[1;31m'
@@ -130,7 +59,7 @@ if [[ "${FROM_VERSION}" == 3.70.2* ]] && [[ "${TO_VERSION}" == 3.73.* ]]; then
     printf "\nFor additional support or questions, feel free to contact hello@cloudogu.com.\n"
 fi
 
-if versionXLessOrEqualThanY "${FROM_VERSION}" "3.68.1-6" && ! versionXLessOrEqualThanY "${TO_VERSION}" "3.73.0-99"; then
+if ! versionXLaterOrEqualThanY "${FROM_VERSION}" "3.70.2" && versionXLaterOrEqualThanY "${TO_VERSION}" "3.73.0-1"; then
     RED='\033[1;31m'
     printf "%s~~~~Warning~~~~\n" "${RED}"
     printf "%sThis update requires a database migration\n"
