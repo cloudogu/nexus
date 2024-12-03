0f92bab2de6552e015a141ae78532511518827c3
Marco Bergen <marco.bergen-extern@cloudogu.com>
2024-12-03 14:10:39 +0100

    [#150] use reencryption as dynamic key
    - add template for secret file
    - add startupstep to create secret file if it does not exist

--- resources/util.sh
+++ resources/util.sh
@@ -90,6 +90,59 @@ EOF
   fi
   echo "enabling groovy scripting"
   echo "nexus.scripts.allowCreation=true" >>${NEXUS_DATA_DIR}/etc/nexus.properties
+
+  setupSecretFile
+
+}
+
+function setupSecretFile() {
+  # See https://help.sonatype.com/en/re-encryption-in-nexus-repository.html
+  # for further instructions
+  SECRET_FILE="${NEXUS_DATA_DIR}/etc/nexus.secrets.json"
+  if [ -f "${SECRET_FILE}" ] && [ -f "${NEXUS_WORKDIR}/resources/nexus.secrets.json.tpl" ]; then
+    echo "Secret-file ${SECRET_FILE} already exists"
+    return
+  fi
+
+  echo "Creating new Security-File for Dynamic Encryption Key"
+
+  # 01 Create the JSON Key File
+  # ---------------------------
+
+  SECRET_ID="nexus-dynamic-secret"
+  SECRET_KEY="$(doguctl random)"
+  # this might be "null" if we are on a multinode system
+  doguctl config "secret_encryption/active" "${SECRET_ID}"
+
+  # to activate match "active" with "id"
+  doguctl config "secret_encryption/id" "${SECRET_ID}"
+  doguctl config "secret_encryption/key" "${SECRET_KEY}"
+
+  # create secret file via template
+  doguctl template "${NEXUS_WORKDIR}/resources/nexus.secrets.json.tpl" "${NEXUS_WORKDIR}/etc/nexus.secrets.json"
+
+  echo " - Securityfile created"
+
+  # 02 Enable Re-Encryption
+  # -----------------------
+
+  # configure Nexus to use this newly created file
+  if grep -Fxq "nexus.secrets.file=" "${NEXUS_DATA_DIR}/etc/nexus.properties"; then
+    echo "Nexus properties already contains a nexus secret file."
+    echo "Check ${NEXUS_DATA_DIR}/etc/nexus.properties"
+    exit 1
+  fi
+  echo "nexus.secrets.file=${SECRET_FILE}" >>${NEXUS_DATA_DIR}/etc/nexus.properties
+
+  echo " - Security-File set in nexus.properties"
+
+  # 03 Create Re-encryption Task via API
+  # ------------------------------------
+
+  # enable encryption after start
+  doguctl config "secret_encryption/start" "1"
+  echo " - Scheduled API-Task after startup"
+
 }
 
 function configureNexusAtFirstStart() {
@@ -131,6 +184,27 @@ function configureNexusAtFirstStart() {
 }
 
 function configureNexusAtSubsequentStart() {
+
+  # Enable re-encryption if set
+  if [ "$(doguctl config 'secret_encryption/start')" == "1" ]; then
+    echo "Start Task for Re-Encryption"
+    echo " - use $(doguctl config -global "fqdn") as fqdn"
+    echo " - use $(doguctl config 'secret_encryption/id') as secretKeyId"
+
+    curl -X 'PUT' \
+      "http://localhost:8081/nexus/service/rest/v1/secrets/encryption/re-encrypt" \
+      -H 'accept: application/json' \
+      -H 'Content-Type: application/json' \
+      -H 'NX-ANTI-CSRF-TOKEN: 1234' \
+      -H 'X-Nexus-UI: true' \
+      -d "{
+            'secretKeyId': '$(doguctl config 'secret_encryption/id')',
+          }"
+
+    # deactivate for next startup
+    doguctl config 'secret_encryption/start' '0'
+  fi
+
   if [ -f "${NEXUS_WORKDIR}/resources/nexusConfigurationSubsequentStart.groovy" ] && [ -f "${NEXUS_WORKDIR}/resources/nexusConfParameters.json.tpl" ]; then
     echo "Getting current admin password"
 
