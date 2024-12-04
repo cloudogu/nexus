f121311d15914b16c25359920816501117a89415
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2024-12-04 14:04:19 +0100

    # 151 download migration helper if not available

--- resources/post-upgrade.sh
+++ resources/post-upgrade.sh
@@ -11,11 +11,11 @@ FROM_VERSION="${1}"
 TO_VERSION="${2}"
 
 if [[ $FROM_VERSION == 3.70.2* ]] && [[ $TO_VERSION == 3.73.0* ]]; then
-  # if [ ! -f "${MIGRATION_HELPER_JAR}" ]; then
-  #   # try to download migration helper jar if it does not exist
-  #   echo "trying to download the migration helper, warum auch immer"
-  #   curl -s -L --retry 3 -o "${MIGRATION_HELPER_JAR}" "https://download.sonatype.com/nexus/nxrm3-migrator/nexus-db-migrator-3.70.3-01.jar"
-  # fi
+  if [ ! -e "${MIGRATION_HELPER_JAR}" ]; then
+    # try to download migration helper jar if it does not exist
+    echo "trying to download the migration helper, warum auch immer"
+    curl -s -L --retry 3 -o "${MIGRATION_HELPER_JAR}" "https://download.sonatype.com/nexus/nxrm3-migrator/nexus-db-migrator-3.70.3-01.jar"
+  fi
   "${NEXUS_WORKDIR}/bin/nexus" stop
   # move the backup artifacts to the workdir because the jar expects them there
   find "${NEXUS_DATA_DIR}" -name "*.bak" -exec mv '{}' "${NEXUS_WORKDIR}" \;
